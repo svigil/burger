59cbfcb8771b5b7a54f150f6638ac93a20b59ba3
Spencer Vigil <spencervigil27@gmail.com>
2018-09-06 00:01:12 -0500

    set up routes and verified that they are working

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,2 +1,10 @@
-var express = require('express');
+// Create Dependencies
+var express = require("express");
 var router = express.Router();
+
+// Set up router
+router.get("/", function(req,res){
+    res.render("index");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-
 // Create Dependencies
-var express = require ('express');
-var bodyParser = require('body-parser');
-var methodoverride = require('method-override');
-var expHbs = require('express-handlebars');
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodoverride = require("method-override");
+var expHbs = require("express-handlebars");
 
 var port = process.env.port || 3000;
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 
-app.use(methodoverride('_method'));
+app.use(methodoverride("_method"));
 
 // Set Handlebars.
-app.engine('handlebars',expHbs({
-    defaultLayout: 'main'
-}));
+app.engine("handlebars", expHbs({defaultLayout: "main" }));
+    
+app.set("view engine", "handlebars");
 
-app.set('view engine','handlebars');
+// Set up routes
+var routes = require("./controllers/burgers_controllers.js");
+app.use("/", routes);
 
 var port = 3000;
 // Start our server so that it can begin listening to client requests.
-app.listen(port,function(){
+app.listen(port, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + port);
 });
